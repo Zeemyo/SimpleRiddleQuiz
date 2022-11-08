99e5f4c627f4f67f18d8a65caa1d07673a52aac6
zeemyo <zidanalam77@gmail.com>
2022-11-08 14:42:01 +0700

    Persona 3

---
+++ app/src/main/java/com/example/simplequiz/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.simplequiz;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Apa yang harus dirusak agar bisa di gunakan?",
+            "Apa yang banyak lubangnya tapi tetap dapat menampung air?",
+            "Jika kamu tau aku, kamu ingin membaginya ke orang lain; jika kamu membaginya kamu tidak bisa menjaga ku. apakah aku?",
+            "Apa yang memiliki banyak kata tapi tidak bisa berbicara?"
+    };
+
+    public static String choices[][] = {
+            {"Telur","Apel","Pintu","Jam","Tanah"},
+            {"Ember","Keranjang","Plastik","Spons","Selang"},
+            {"Souvenir","Rahasia","Uang","Kekasih","Kue"},
+            {"Speaker","Musik","Buku","Pohon", "Kloset"}
+    };
+
+    public static String correctAnswers[] = {
+            "Telur",
+            "Spons",
+            "Rahasia",
+            "Buku"
+    };
+
+}
